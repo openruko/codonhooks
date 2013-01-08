419b2057d060ec6e4dbfea80168be0400cd1c660
Filirom1 <filirom1@gmail.com>
2013-01-08 09:39:15 +0100

    add php test

---
+++ test/php-test.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+php_example="/tmp/checkout"
+
+rm -fr $php_example
+mkdir -p $php_example
+
+cat >> "$php_example/index.php" <<EOF
+  I have <?= 1+1 ?> foo.
+EOF
+
+cat >> "$php_example/Procfile" <<EOF
+web: sh boot.sh
+EOF
+
+dir=`pwd`
+cd $php_example
+git init
+git add -A
+git commit -m "first commit"
+cd $dir
+
+expect << EOF
+  spawn ./pre-receive-launcher.sh
+  expect "PHP app detected"
+  expect "Bundling Apache version"
+  expect "Bundling PHP version"
+  expect "Discovering process types"
+  expect "Default process types for PHP -> web"
+  expect "Compiled slug size is "
+  expect "Using slug_id: 1234"
+  expect eof
+EOF
+
+rm -fr /app/*
+mv /tmp/checkout/* /app
+
+cd /app
+cat >> .env << EOF
+PATH=bin:$PATH
+PORT=9999
+EOF
+
+foreman start
+
+expect << EOF
+  spawn curl localhost:9999
+  expect "I have 2 foo."
+  expect eof
+EOF
--- test/pre-receive-launcher.sh
+++ test/pre-receive-launcher.sh
@@ -8,15 +8,6 @@ example=$1
 export GIT_WORK_TREE=
 export dyno_web_url="test.mymachine.me"
 export slug_id=1234
-export push_code_url="http://127.0.0.1:15612/push"
-
-node & <<EOF
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('v42\n');
-}).listen(15612);
-EOF
 
 BUILDPACKS_DIR="/tmp/buildpacks"
 if [ ! -d "$BUILDPACKS_DIR/buildpacks" ]; then
@@ -25,12 +16,11 @@ if [ ! -d "$BUILDPACKS_DIR/buildpacks" ]; then
   tar xvzf "$BUILDPACKS_DIR/buildpacks.tgz" -C "$BUILDPACKS_DIR"
 fi
 
-rm -fr "/tmp/checkout"
 rm -fr "/tmp/cache"
-cp -r "fixture/$example" "/tmp/checkout"
 
 OLDREV="master"
 NEWREV="master"
 REF="refs/heads/master"
 
 ../pre-receive <<< "$OLDREV $NEWREV $REF"
+
